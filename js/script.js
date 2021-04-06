4af48a3bc0b533d08113cae79050efaa49f2a5b7
Sennarion <janadine.pro@gmail.com>
2021-04-06 22:27:06 +0300

    Create First Commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+  $(".header__burger").click(function () {
+        $(this).toggleClass('header__burger-active');
+         $(".intro__nav-wrapper").toggleClass("active");
+       });
+    
+
+});
